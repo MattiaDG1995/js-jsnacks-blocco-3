9c713200abca8e3c60b680df921363b7446e757f
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-10-26 15:14:48 +0200

    snack2

---
+++ main.js
@@ -0,0 +1,56 @@
+// *Snack1*
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+// *Snack2*
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// *Snack3* (Bonus)
+// Fai inserire un numero, che chiameremo N, all’utente.
+// Genera N array,
+// ognuno formato da 10 numeri casuali da 1 a 100.
+// Ogni volta che ne crei uno, stampalo.
+// *Snack4 (Bonus)*
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+// Interrompi il ciclo appena il nome è stato trovato.
+
+// snack1
+
+// let numero = [];
+// let usernumber ;
+
+// while( sum(numero) < 50  ){
+//     usernumber=parseInt(prompt('inserisci numero'))
+//     numero.push(usernumber)
+// }
+
+// function sum(array){ 
+
+//     let numbersum = 0;
+
+//     for(let i = 0; i < array.length; i++){
+
+//         numbersum += array[i];
+//     }
+
+//     return numbersum
+// }
+
+
+
+// snack2
+
+
+// let num = []; 
+// let somma = 0;
+// let i = 0;
+
+//     while(i < 5){  
+
+//         somma += parseInt(prompt(`inserisci num`))
+//         i++
+
+//       }
+
+//       console.log(somma)
+
+   
